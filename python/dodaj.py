7903c430f8d9406e9d8f5f5b4b7e97d762d28336
twojojciec <wojciech.bryla@lo1.sandomierz.pl>
2019-10-23 08:43:42 +0200

    dodanie pierwszego skryptu

---
+++ python/dodaj.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+
+
+def main(args):
+    print('witaj python')
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
